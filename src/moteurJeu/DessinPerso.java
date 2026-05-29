2499ab211164799cb81aea65c642a0350e7c9795
Maxime SCHORSCH-LEGRAND <maxime.schorsch-legrand3@etu.univ-lorraine.fr>
2026-05-29 11:30:17 +0200

    pam

--- src/moteurJeu/DessinPerso.java
+++ src/moteurJeu/DessinPerso.java
@@ -8,13 +8,13 @@ import java.awt.image.BufferedImage;
 public class DessinPerso implements DessinJeu{
     JeuPerso jeu;
 
+    public void  dessiner(BufferedImage im){
 
-
-    @Override
-    public void dessiner(BufferedImage image) {
         Graphics g = im.getGraphics();
 
         g.drawRectangle(jeu.x*50,jeu.y*50,
-                jeu.x*50+10, jeu,y*50+10);
+        jeu.x*50+10, jeu,y*50+10);
+
     }
+    
 }
