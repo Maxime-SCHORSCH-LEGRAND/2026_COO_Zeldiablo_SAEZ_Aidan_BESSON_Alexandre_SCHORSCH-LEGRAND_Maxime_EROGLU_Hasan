1fd580b0c9b05c41e0f73aadd1fc1fa90ed43855
Alexandre Besson <alexandre.besson1@etu.univ-lorraine.fr>
2026-05-29 16:32:31 +0200

    regle beugue

--- src/main/Hero.java
+++ src/main/Hero.java
@@ -3,12 +3,12 @@ package main;
 public class Hero extends Personnage{
 
     private Position pos;
-<<<<<<< HEAD
+
     public int degat=5;
     int d;
-=======
+
     public int vie ;
->>>>>>> 77e8effb9d5b4e4d0d45f788bace755686a29b6f
+
 
     public Hero(int x, int y , int vie ) {
         super(x,y,vie);
