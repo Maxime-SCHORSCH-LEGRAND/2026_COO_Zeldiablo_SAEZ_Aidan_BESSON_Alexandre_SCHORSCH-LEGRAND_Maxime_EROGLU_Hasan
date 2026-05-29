4684040842cd26a0fd63e2d45c475fa5223a666d
Maxime SCHORSCH-LEGRAND <maxime.schorsch-legrand3@etu.univ-lorraine.fr>
2026-05-29 11:09:03 +0200

    répar

--- src/main/Hero.java
+++ src/main/Hero.java
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-public class Hero {
 
 
-
-=======
 package main;
 
 public class Hero extends Personnage {
@@ -12,15 +8,5 @@ public class Hero extends Personnage {
         super(x, y, 20);
     }
 
-    public void attaquer(Monstre m) {
 
-        int dx = Math.abs(pos.x - m.getPos().x);
-        int dy = Math.abs(pos.y - m.getPos().y);
-
-        if(dx + dy == 1) {
-            m.subirDegats(5);
-            System.out.println("Le héros attaque !");
-        }
-    }
->>>>>>> fe8e1875f976b58b7431dc207cde4be5956aca3a
 }
