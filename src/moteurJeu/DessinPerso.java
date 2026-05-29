3edf76092732a8bfdf2d554c4392bca59e66e626
Alexandre Besson <alexandre.besson1@etu.univ-lorraine.fr>
2026-05-29 11:29:05 +0200

    h

--- src/moteurJeu/DessinPerso.java
+++ src/moteurJeu/DessinPerso.java
@@ -1,10 +1,11 @@
 package moteurJeu;
 
+import java.awt.*;
+
 public class DessinPerso implements DessinJeu{
     JeuPerso jeu;
 
     void dessiner(BufferedImageim){
-        //Graphics
         Graphics g = im.getGraphics();
 
         g.drawRectangle(jeu.x*50,jeu.y*50,
